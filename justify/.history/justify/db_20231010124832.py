e63a2f4d06fc1f0076a7979d2dd3ef4d3f9948d7
AlejandroJesusSerrano <alejandrojserrano@hotmail.com>
2023-10-10 15:00:19 -0300

    first commit

---
+++ justify/.history/justify/db_20231010124832.py
@@ -0,0 +1,13 @@
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+MYSQL = {
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'justify',
+    'USER': 'root',
+    'PASSWORD': 'C0mpu705F3d3r4l23+',
+    'HOST': 'localhost',
+    'PORT': '3306',
+  }
+}
+
---
+++ justify/.history/justify/db_20231010125053.py
@@ -0,0 +1,11 @@
+MYSQL = {
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'justify',
+    'USER': 'root',
+    'PASSWORD': 'C0mpu705F3d3r4l23+',
+    'HOST': 'localhost',
+    'PORT': '3306',
+  }
+}
+
